4ded746b2b9c660866af27f16f41230485faeeb8
nulisbuku <dimasalifarsyad126@gmail.com>
2025-10-30 11:10:07 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 const tombolSapa = document.getElementById('sapaButton')
-tombolSapa.addEventListener('click', function() {
+tombolSapa.addEventListener('click', function(){
   alert('Halo! Terima kasih, salam kenal!')
 });
